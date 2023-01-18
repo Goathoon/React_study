099a484c2f2ae71e4bc10be736df5521a754a04b
DESKTOP-MIDQG7C\태훈 <ownest2@gmail.com>
2023-01-18 11:42:59 +0900

    Redux 부연 설명 (주석) 추가

--- src/modules/auth.js
+++ src/modules/auth.js
@@ -1,5 +1,5 @@
 import {createAction,handleActions} from 'redux-actions';
-const SAMPLE_ACTION = 'auth/SAMPLE_ACTION';
+const SAMPLE_ACTION = 'auth/SAMPLE_ACTION'; //auth 모듈 안에 SAMPLE_ACTION이라는 액션타입이 존재한다.(관습적인 룰)
 export const sampleAction = createAction(SAMPLE_ACTION);
 const initialState = {};
 
